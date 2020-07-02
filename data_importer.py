0e08f2c04b9b692b79bfb284f952728b43c236c2
ben bridges <benbridges@bens-mbp.lan>
2020-07-02 15:17:15 -0500

    Test initial import and transformation of files

--- data_importer.py
+++ data_importer.py
@@ -1,24 +1,66 @@
 import re
+import pandas as pd
+from pandas.core.common import flatten
+from datetime import datetime
+
+class InvalidFileFormatException(Exception):
+    pass
 
 class InvalidFileExtensionException(Exception):
     pass
 
+class InvalidHeaderWithDateFormatException(Exception):
+    pass
+
 class DataImporter():
-    PERMITTED_FILE_EXTENSIONS = [".txt", ".xlsx"]
+    PERMITTED_FILE_EXTENSIONS = ['.txt', '.xlsx']
 
     FILE_EXTENSIONS_REGEX = re.compile(r'({})$'.format(
         '|'.join(re.escape(x) for x in PERMITTED_FILE_EXTENSIONS)
     ))
 
-    def load(self, file_path):
-        if self.is_valid_file_extension(file_path):
-            if file_path.endswith(".xlsx"):
-                return "Excel file"
+    MASTER_SCHEMA = ['Year','Month','SKU','Category','Units','Gross Sales']
 
-            if file_path.endswith(".txt"):
-                return "Text file"
+    def load_as_dataframe_with_schema(self, file_path):
+        new_rows = []
+        if self.FILE_EXTENSIONS_REGEX.search(file_path):
+            df = self.get_file_as_dataframe(file_path)
+            for i, row in df.iterrows():
+                row_as_dict = {}
+                for data_tuple in row.iteritems():
+                    header = data_tuple[0]
+                    value = data_tuple[1]
+                    if header not in ['SKU', 'Section']:
+                        date_and_key_tuple = self.get_date_and_key_tuple(header)
+                        date = date_and_key_tuple[0]
+                        key = date_and_key_tuple[1]
+                        if date not in row_as_dict:
+                            year_and_month = date.split('-')
+                            row_as_dict[date] = {
+                                'Year': year_and_month[0],
+                                'Month': year_and_month[1],
+                                'SKU': row.SKU,
+                                'Category': row.Section
+                            }
+                        row_as_dict[date][key] = value
+                new_rows.append(row_as_dict.values())
+
+            return pd.DataFrame(
+                [r for row in new_rows for r in row], 
+                columns=self.MASTER_SCHEMA
+            )
         else:
             raise InvalidFileExtensionException
 
-    def is_valid_file_extension(self, file_path):
-        return bool(self.FILE_EXTENSIONS_REGEX.search(file_path))
+    def get_date_and_key_tuple(self, key):
+        date_and_key = key.split(' ', 1)
+        if len(date_and_key) == 2:
+            return (date_and_key[0], date_and_key[1])
+        else:
+            raise InvalidHeaderWithDateFormatException
+
+    def get_file_as_dataframe(self, file_path):
+        if file_path.endswith('.xlsx'):
+            return pd.read_excel(file_path)
+        elif file_path.endswith('.txt'):
+            return pd.read_csv(file_path, sep='\t', lineterminator='\r')
--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,22 +1,50 @@
 import unittest
-from data_importer import DataImporter, InvalidFileExtensionException
+from data_importer import DataImporter, InvalidFileExtensionException, InvalidHeaderWithDateFormatException
+from xlrd import XLRDError
+from pandas.errors import ParserError
 
 class TestDataImporter(unittest.TestCase):
     def setUp(self):
         self._class = DataImporter()
 
-    def test_load_as_dataframe_with_txt_file(self):
-        df = self._class.load_as_dataframe("tests/fixtures/test-201905.txt")
+    # TESTS LOADING DATAFRAME USING MASTER SCHEMA
+    def test_load_as_dataframe_with_schema_using_txt_file(self):
+        df = self._class.load_as_dataframe_with_schema('tests/fixtures/test-201905.txt')
         # given files with 4 rows and 6 unique dates in multiple columns
         self.assertEqual(len(df.values), 24)
 
-    def test_load_as_dataframe_with_xlsx_file(self):
-        df = self._class.load_as_dataframe("tests/fixtures/test-201904.xlsx")
+    def test_load_as_dataframe_with_schema_using_xlsx_file(self):
+        df = self._class.load_as_dataframe_with_schema('tests/fixtures/test-201904.xlsx')
         # given files with 4 rows and 6 unique dates in multiple columns
         self.assertEqual(len(df.values), 24)
 
     def test_raises_exception_with_invalid_file_extension(self):
-        self.assertRaises(InvalidFileExtensionException, self._class.load_as_dataframe, "foo.md")
+        self.assertRaises(InvalidFileExtensionException, self._class.load_as_dataframe_with_schema, 'foo.md')
+
+    # TESTS GET FILE AS DATAFRAME
+    def test_get_file_as_dataframe_with_txt_file(self):
+        df = self._class.get_file_as_dataframe('tests/fixtures/test-201905.txt')
+        # given files with 4 rows
+        self.assertEqual(len(df.values), 4)
+
+    def test_get_file_as_dataframe_with_xlsx_file(self):
+        df = self._class.get_file_as_dataframe('tests/fixtures/test-201904.xlsx')
+        # given files with 4 rows
+        self.assertEqual(len(df.values), 4)
+
+    def test_raises_exception_with_invalid_file_format_get_file_as_dataframe_xlsx(self):
+        self.assertRaises(XLRDError, self._class.get_file_as_dataframe, 'tests/fixtures/test-201905-bad-format.xlsx')
+
+    def test_raises_exception_with_invalid_file_format_get_file_as_dataframe_txt(self):
+        self.assertRaises(ParserError, self._class.get_file_as_dataframe, 'tests/fixtures/test-201904-bad-format.txt')
+
+    # TESTS GET DATE AND KEY TUPLE
+    def test_get_date_and_key_tuple(self):
+        date_and_key_tuple = self._class.get_date_and_key_tuple('2020-1 Units')
+        self.assertEqual(date_and_key_tuple, ('2020-1', 'Units'))
+
+    def test_raises_exception_with_invalid_data_get_date_and_key_tuple(self):
+        self.assertRaises(InvalidHeaderWithDateFormatException, self._class.get_date_and_key_tuple, '2020-1Units')
 
 if __name__ == '__main__':
     unittest.main()
