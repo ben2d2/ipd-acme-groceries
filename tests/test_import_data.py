9ff08d4812851c205f4a9c86bb5f7d47a3f511b5
ben bridges <benbridges@bens-mbp.lan>
2020-07-02 13:40:23 -0500

    Add tests for pandas import and transformation

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -5,14 +5,18 @@ class TestDataImporter(unittest.TestCase):
     def setUp(self):
         self._class = DataImporter()
 
-    def test_load_data_with_txt_file(self):
-        self.assertEqual(self._class.load("foo.txt"), "Text file")
+    def test_load_as_dataframe_with_txt_file(self):
+        df = self._class.load_as_dataframe("tests/fixtures/test-201905.txt")
+        # given files with 4 rows and 6 unique dates in multiple columns
+        self.assertEqual(len(df.values), 24)
 
-    def test_load_data_with_xlsx_file(self):
-        self.assertEqual(self._class.load("foo.xlsx"), "Excel file")
+    def test_load_as_dataframe_with_xlsx_file(self):
+        df = self._class.load_as_dataframe("tests/fixtures/test-201904.xlsx")
+        # given files with 4 rows and 6 unique dates in multiple columns
+        self.assertEqual(len(df.values), 24)
 
     def test_raises_exception_with_invalid_file_extension(self):
-        self.assertRaises(InvalidFileExtensionException, self._class.load, "foo.md")
+        self.assertRaises(InvalidFileExtensionException, self._class.load_as_dataframe, "foo.md")
 
 if __name__ == '__main__':
     unittest.main()
