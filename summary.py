478dc6aa0a777f6e79c7b0e777943f9ccb6a5e1b
ben bridges <benbridges@bens-mbp.lan>
2020-07-06 11:40:23 -0500

    Test reading csv file and removing dupes

--- summary.py
+++ summary.py
@@ -3,11 +3,15 @@ class Summary():
 		self.dataframe = dataframe
 
 	def calculate_for(self, category, year, month):
-		df = self.dataframe.drop_duplicates(subset=self.dataframe.columns.difference(['ImportedAt']))
-		results = df[(df.Category == category) & (df.Year == year) & (df.Month == month)]
+		df = self.dataframe
+		results = df[
+			(df.Category == category) & (df.Year == int(year)) & (df.Month == int(month))
+		].sort_values(by='ImportedAt').drop_duplicates(
+			subset=['SKU', 'Year', 'Month'], keep='last'
+		)
 		if len(results) > 0:
 			total_units = results['Units'].sum()
-			total_gross_sales = results['Gross Sales'].sum()
-			return "%s - Total Units: %s, Total Gross Sales: %s" % (category, total_units, total_gross_sales)
+			total_sales = results['Gross Sales'].sum()
+			return "%s - Total Units: %s, Total Gross Sales: %s" % (category, total_units, total_sales)
 		else:
 			return 'No data available'
--- tests/test_summary.py
+++ tests/test_summary.py
@@ -1,15 +1,22 @@
 import logging
 import unittest
+import os
+import pandas as pd
 from data_importer import DataImporter
 from summary import Summary
 
 class TestSummary(unittest.TestCase):
     def setUp(self):
         data_importer = DataImporter()
-        dataframe = data_importer.load_as_dataframe_with_schema('tests/fixtures/test-201905.txt')
+        data_importer.load_as_dataframe_with_schema('tests/fixtures/test-201905.txt')
+        dataframe = pd.read_csv('master.csv')
+        dataframe.set_index(['ImportedAt', 'Year', 'Month', 'Category'])
         self._class = Summary(dataframe)
         # disable logging
         logging.disable(logging.CRITICAL)
+
+    def tearDown(self):
+        os.remove('master.csv')
 
     # TESTS LOADING DATAFRAME USING MASTER SCHEMA
     def test_calculate_for(self):
@@ -22,7 +29,9 @@ class TestSummary(unittest.TestCase):
 
     def test_calculate_for_with_duplicates(self):
         data_importer = DataImporter()
-        dataframe = data_importer.load_as_dataframe_with_schema('tests/fixtures/test-201905-with-dupes.txt')
+        data_importer.load_as_dataframe_with_schema('tests/fixtures/test-201905-with-dupes.txt')
+        dataframe = pd.read_csv('master.csv')
+        dataframe.set_index(['ImportedAt', 'Year', 'Month', 'Category'])
         summary = Summary(dataframe)
         result = summary.calculate_for('Produce', '2018', '12')
         self.assertEqual(result, 'Produce - Total Units: 717, Total Gross Sales: 11658.83')
