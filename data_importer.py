28124fe61c2764bf3eb80272a2b2f48600872595
ben bridges <benbridges@bens-mbp.lan>
2020-07-06 10:10:53 -0500

    Reorder columns

--- data_importer.py
+++ data_importer.py
@@ -14,7 +14,7 @@ class DataImporter():
         '|'.join(re.escape(x) for x in PERMITTED_FILE_EXTENSIONS)
     ))
 
-    MASTER_SCHEMA = ['Year','Month','ImportedAt','SKU','Category','Units','Gross Sales']
+    MASTER_SCHEMA = ['Year','Month','SKU','Category','Units','Gross Sales','ImportedAt']
 
     def load_as_dataframe_with_schema(self, file_path):
         new_rows = []
@@ -32,9 +32,9 @@ class DataImporter():
                                 row_as_dict[date] = {
                                     'Year': year,
                                     'Month': month,
-                                    'ImportedAt': datetime.now(),
                                     'SKU': row.SKU,
-                                    'Category': row.Section
+                                    'Category': row.Section,
+                                    'ImportedAt': datetime.now()
                                 }
                             row_as_dict[date][key] = int(value) if key == 'Units' else float(value)
                         except:
