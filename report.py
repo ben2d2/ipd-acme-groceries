be0856ea609256541cdc6e4ddfe9c9e170231131
ben bridges <benbridges@bens-mbp.lan>
2020-07-07 16:55:03 -0500

    Add try and exception logging in summary and report

--- report.py
+++ report.py
@@ -1,12 +1,17 @@
 import pandas as pd
+import logging
 
 class Report():
 	def __init__(self, dataframe):
 		self.dataframe = dataframe
 
 	def gather_data(self):
-		df = self.dataframe.sort_values(by='ImportedAt').drop_duplicates(
-			subset=['SKU', 'Year', 'Month'], keep='last'
-		)
-		results = df[(df['Units'] != 0) & (df['Gross Sales'] != 0)].drop(columns=['ImportedAt'], axis=1)
-		return results.sort_values(by=['SKU', 'Year', 'Month'])
+		try:
+			df = self.dataframe.sort_values(by='ImportedAt').drop_duplicates(
+				subset=['SKU', 'Year', 'Month'], keep='last'
+			)
+			results = df[(df['Units'] != 0) & (df['Gross Sales'] != 0)].drop(columns=['ImportedAt'], axis=1)
+			return results.sort_values(by=['SKU', 'Year', 'Month'])
+		except Exception as error:
+			logging.error(error)
+			raise error
--- summary.py
+++ summary.py
@@ -1,17 +1,23 @@
+import logging
+
 class Summary():
 	def __init__(self, dataframe):
 		self.dataframe = dataframe
 
 	def calculate_for(self, category, year, month):
-		df = self.dataframe
-		results = df[
-			(df.Category == category) & (df.Year == int(year)) & (df.Month == int(month))
-		].sort_values(by='ImportedAt').drop_duplicates(
-			subset=['SKU', 'Year', 'Month'], keep='last'
-		)
-		if len(results) > 0:
-			total_units = results['Units'].sum()
-			total_sales = round(results['Gross Sales'].sum(), 2)
-			return "%s - Total Units: %s, Total Gross Sales: %s" % (category, total_units, total_sales)
-		else:
-			return 'No data available'
+		try:
+			df = self.dataframe
+			results = df[
+				(df.Category == category) & (df.Year == int(year)) & (df.Month == int(month))
+			].sort_values(by='ImportedAt').drop_duplicates(
+				subset=['SKU', 'Year', 'Month'], keep='last'
+			)
+			if len(results) > 0:
+				total_units = results['Units'].sum()
+				total_sales = round(results['Gross Sales'].sum(), 2)
+				return "%s - Total Units: %s, Total Gross Sales: %s" % (category, total_units, total_sales)
+			else:
+				return 'No data available'
+		except Exception as error:
+			logging.error(error)
+			raise error
